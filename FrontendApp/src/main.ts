e261ed41f12a83f828043ed6616fb737b200fcb4
Oleksandr Nekrashevych <shonnekrashevych@gmail.com>
2025-01-08 00:52:34 +0200

    added routing to webpage

--- FrontendApp/src/main.ts
+++ FrontendApp/src/main.ts
@@ -8,6 +8,13 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideRouter([
       { path: '', component: HomeComponent },
+      { path: 'home', component: HomeComponent },
+      { path: 'about', component: HomeComponent },
+      { path: 'achievements', component: HomeComponent },
+      { path: 'projects', component: HomeComponent },
+      { path: 'skills', component: HomeComponent },
+      { path: 'resume', component: HomeComponent },
+      { path: 'contact', component: HomeComponent },
     ]),
   ],
 }).catch(err => console.error(err));
